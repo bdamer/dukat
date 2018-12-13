010210507e84ef7dde4e279292fc078ac76d5919
Ben Damer <bdamer@gmail.com>
2018-12-12 23:29:02 -0800

    Added methods to multiply colors and check for equality

--- include/dukat/color.h
+++ include/dukat/color.h
@@ -9,7 +9,8 @@ namespace dukat
 		Color operator+(const Color& c) const { return { r + c.r, g + c.g, b + c.b, a + c.a }; }
 		Color operator-(const Color& c) const { return { r - c.r, g - c.g, b - c.b, a - c.a }; }
 		Color operator*(float s) const { return { r * s, g * s, b * s, a * s }; }
-		Color operator/(float s) const { float one_over_s = 1.0f / s; return{ r * one_over_s, g * one_over_s, b * one_over_s, a * one_over_s }; }
+		Color operator/(float s) const { auto one_over_s = 1.0f / s; return{ r * one_over_s, g * one_over_s, b * one_over_s, a * one_over_s }; }
+		bool operator==(const Color& c) const { return (r == c.r) && (g == c.g) && (b == c.b) && (a == c.a); }
 	};
 
 	inline Color& operator+=(Color& a, const Color& b)
@@ -30,9 +31,27 @@ namespace dukat
 		return a;
 	}
 
+	inline Color& operator*=(Color& a, const Color& b)
+	{
+		a.r *= b.r;
+		a.g *= b.g;
+		a.b *= b.b;
+		a.a *= b.a;
+		return a;
+	}
+
 	inline Color& operator/=(Color& a, float scalar)
 	{
 		a = a / scalar;
+		return a;
+	}
+
+	inline Color& operator/=(Color& a, const Color& b)
+	{
+		a.r /= b.r;
+		a.g /= b.g;
+		a.b /= b.b;
+		a.a /= b.a;
 		return a;
 	}
 
