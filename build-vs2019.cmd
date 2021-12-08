cfa684375a1b82215c6c1011a5d6da8488e11cf4
Ben Damer <bdamer@gmail.com>
2021-12-07 16:24:40 -0800

    Updated toolkit version

--- build-vs2019.cmd
+++ build-vs2019.cmd
@@ -4,7 +4,7 @@ set VSPATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
 set DEVENV=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe
 
 echo Setting up environment
-call "%VSPATH%\VC\Auxiliary\Build\vcvarsall.bat" x86 8.1 -vcvars_ver=14.0
+call "%VSPATH%\VC\Auxiliary\Build\vcvarsall.bat" x86 10.0.19041.0 -vcvars_ver=14.29
 
 echo Cleaning up last build
 "%DEVENV%" .\vs2015\dukat.sln /clean "Release"
