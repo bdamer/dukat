5fbdcdd5ee31cc05e2d08b6d9dcd53d4b7e21e40
Ben Damer <bdamer@gmail.com>
2023-10-03 16:46:58 -0700

    Create parameter for build target.

--- build-vs2019.cmd
+++ build-vs2019.cmd
@@ -1,17 +1,20 @@
 @echo off
 
+set TARGET=%1
 set VSPATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
 set DEVENV=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe
+
+if "%TARGET%"=="" GOTO ERROR
 
 echo Setting up environment
 call "%VSPATH%\VC\Auxiliary\Build\vcvarsall.bat" x86 10.0.19041.0 -vcvars_ver=14.29
 
 echo Cleaning up last build
-"%DEVENV%" .\vs2015\dukat.sln /clean "Release|x86"
+"%DEVENV%" .\vs2015\dukat.sln /clean "%TARGET%"
 if NOT ERRORLEVEL 0 GOTO ERROR
 
 echo Building dukat
-"%DEVENV%" .\vs2015\dukat.sln /build "Release|x86"
+"%DEVENV%" .\vs2015\dukat.sln /build "%TARGET%"
 if NOT ERRORLEVEL 0 GOTO ERROR
 
 echo Build complete
